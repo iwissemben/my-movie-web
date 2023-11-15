33358a73f77941178c0c45cc96277f5de98ffc54
Wissem Ben <107781875+iwissemben@users.noreply.github.com>
2023-11-15 18:28:39 +0100

    Update config.js
    FIX: proxy url

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://wiss-movie-web.w-issame.workers.dev",
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDgzYzAyMDMxOTM5MDc5NTgxZTljMjk0OWY5ODQ2YyIsInN1YiI6IjY1NTRlZTNiNjdiNjEzNDVjZTM4MTJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aCz7nx5yEon0IBXQenMllFzB9BNlIVz34-yx8h4XQcY"
-
+  VITE_CORS_PROXY_URL: "https://wissous.w-issame.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDgzYzAyMDMxOTM5MDc5NTgxZTljMjk0OWY5ODQ2YyIsInN1YiI6IjY1NTRlZTNiNjdiNjEzNDVjZTM4MTJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aCz7nx5yEon0IBXQenMllFzB9BNlIVz34-yx8h4XQcY",
 };
