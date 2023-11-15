3becf059235305f033485f4478b760b32bd04c71
Wissem Ben <107781875+iwissemben@users.noreply.github.com>
2023-11-15 18:06:54 +0100

    Update config.js
    - fix: API key

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://wiss-movie-web.w-issame.workers.dev",
-  VITE_TMDB_READ_API_KEY: "e483c02031939079581e9c2949f9846c"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDgzYzAyMDMxOTM5MDc5NTgxZTljMjk0OWY5ODQ2YyIsInN1YiI6IjY1NTRlZTNiNjdiNjEzNDVjZTM4MTJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aCz7nx5yEon0IBXQenMllFzB9BNlIVz34-yx8h4XQcY"
+
 };
