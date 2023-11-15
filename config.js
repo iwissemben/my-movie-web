664f10c05eb16a7507df1b22dc9c994c70241304
Wissem Ben <107781875+iwissemben@users.noreply.github.com>
2023-11-15 17:29:25 +0100

    client step setup
    final client step

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://wiss-movie-web.w-issame.workers.dev",
+  VITE_TMDB_READ_API_KEY: "e483c02031939079581e9c2949f9846c"
+};
